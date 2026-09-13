0c1f076cdb4a535753c55ea36af36b794fa1e644
ferdinanduspakasi <ferdinandus.pakasi@ui.ac.id>
2026-09-13 15:47:39 +0700

    fix: typing error in models.py

--- main/models.py
+++ main/models.py
@@ -35,18 +35,19 @@ class Experience(models.Model):
     # Education
     class Education(models.Model):
         DEGREE_CHOICES = [
-            ('junior-high-school', 'Middle School')
+            ('junior-high-school', 'Middle School'),
             ('high-school', 'High School'),
             ('diploma', 'Diploma'),
             ('bachelor', "Bachelor's"),
             ('master', "Master's"),
             ('doctorate', 'Doctorate'),
             ('certification', 'Certification'),
-    ]
+        ]
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     institution_name = models.CharField(max_length=255)
-    degree = models.CharField(max_length=20, choices=DEGREE_CHOICES, default='bachelor')
+    degree = models.CharField(
+        max_length=20, choices=DEGREE_CHOICES, default='bachelor')
     field_of_study = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     started_at = models.DateTimeField(auto_now_add=True)
