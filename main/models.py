2d1eff05fce522818d5be60cbd52f559d87cd4e3
ferdinanduspakasi <ferdinandus.pakasi@ui.ac.id>
2026-09-14 10:44:05 +0700

    feat: implement education HTML and add image URL field in models.py

--- main/models.py
+++ main/models.py
@@ -33,6 +33,8 @@ class Experience(models.Model):
         return self.ended_at is None
 
     # Education
+
+
 class Education(models.Model):
     DEGREE_CHOICES = [
         ('junior-high-school', 'Middle School'),
@@ -49,6 +51,7 @@ class Education(models.Model):
     degree = models.CharField(
         max_length=20, choices=DEGREE_CHOICES, default='bachelor')
     field_of_study = models.CharField(max_length=255)
+    logo_url = models.URLField(blank=True, null=True)
     description = models.TextField(blank=True)
     started_at = models.DateTimeField(auto_now_add=True)
     ended_at = models.DateTimeField(blank=True, null=True)
